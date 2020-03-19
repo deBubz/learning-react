444e461d4e387cbd1f9cb53d7ff396ccd3c713e4
debubz <bao.es.hoang2@gmail.com>
2020-03-19 17:07:29 +1100

    added "add new ex"

--- learning/mern-ex-tracker/src/components/ex-create.components.js
+++ learning/mern-ex-tracker/src/components/ex-create.components.js
@@ -1,7 +1,110 @@
 import React from 'react';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default class ExCreate extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+ 
+    // set state for testing
+    componentDidMount() {
+        this.setState({
+            users: ['test', 'basic'],
+            username: 'test',
+        });
+    }
+
+    // On Change Methods
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value,
+        });
+    }
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value,
+        });
+    }
+    onChangeDuration(e) {
+        this.setState({
+            Duration: Number(e.target.value),
+        });
+    }
+    onChangeDate(date) {
+        this.setState({
+            date: date,
+        });
+    }
+
+    // submit form
+    onSubmit(e) {
+        e.preventDefault();
+
+        // new object to add
+        const exercise = {
+            username: this.state.username,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date,
+        }
+
+        // add
+        console.log(exercise);
+        window.location = '/';      // redirect after adding
+    }
+
     render() {
-        return <div>ExCreate</div>
+        return(
+            <div className='container'>
+                <h3>Add Exercise log</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Username: </label>
+                        <select ref="UserInput" required className='form-control'
+                            value={this.state.name} onChange={this.onChangeUsername}>
+                                {
+                                    this.state.users.map(user => {
+                                        return <option key={user} value={user}>{user}</option>
+                                    })
+                                }
+                            </select>
+                    </div>
+                    <div className='form-group'>
+                        <label>Description: </label>
+                        <input type='text' required className='form-control'
+                            value={this.state.description} onChange={this.onChangeDescription} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Duration: </label>
+                        <input type='text' required className='form-control'
+                            value={this.state.duration} onChange={this.onChangeDuration} />
+                    </div>
+                    <div className='form-group'>
+                        <label>Date: </label>
+                        <DatePicker selected={this.state.date} onChange={this.onChangeDate} />
+                    </div>
+
+                    <div className='form-group'>
+                        <input type='submit' value='Create Exercise Log' className='btn btn-primary' />
+                    </div>
+                </form>
+
+            </div>
+        ) 
     }
 }
