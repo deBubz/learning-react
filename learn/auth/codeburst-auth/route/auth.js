e6750daf2c6062ddcb1250423e1e6cb363a2c127
bubz <bao.es.hoang2@gmail.com>
2020-09-07 18:25:23 +1000

    holycrap it works

--- learn/auth/codeburst-auth/route/auth.js
+++ learn/auth/codeburst-auth/route/auth.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // import model
 const User = require("../model/User");
-const { registerValidation } = require("../validation");
-const Joi = require("joi");
+const { registerValidation, loginValidation } = require("../validation");
+const { valid, required } = require("joi");
 
 // register path
 router.post("/register", async (req, res) => {
@@ -14,16 +16,20 @@ router.post("/register", async (req, res) => {
     }
     
     // check existing email
-    const isEmailExist = await user.findOne({ email: req.body.email });
+    const isEmailExist = await User.findOne({ email: req.body.email });
     if(isEmailExist) {
         return res.status(400).json({ error: "Email already exists" });
     }
+
+    // password hashing
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
 
     // user model object
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password,
     });
 
     // try catch save
@@ -36,5 +42,40 @@ router.post("/register", async (req, res) => {
         res.status(400).json({error});
     }
 });
+
+// login path
+router.post("/login", async (req, res) => {
+    // validate user
+    const { error } = loginValidation(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    // check matching email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).json({ error: "WRONG EMAIL" });
+    }
+
+    // check password
+    const validatePassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validatePassword) {
+        return res.status(400).json({ error: "WRONG PASSWORD" });
+    }
+
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id,
+        }, process.env.TOKEN_SECRET
+    );
+
+    res.json({
+        error: null,
+        data: {
+            token,
+            message: "YOURE IN",
+        }
+    })
+})
 
 module.exports = router;
--- learn/auth/codeburst-auth/validation.js
+++ learn/auth/codeburst-auth/validation.js
@@ -1,6 +1,6 @@
 const { required } = require("joi");
 
-const Joi = require("hapi");
+const Joi = require("joi");
 
 const registerValidation = data => {
     const schema = Joi.object({
@@ -12,6 +12,17 @@ const registerValidation = data => {
     return schema.validate(data);
 }
 
+const loginValidation = data => {
+    const schema = Joi.object({
+        // name: Joi.string().min(6).max(255).required(),
+        email: Joi.string().min(6).max(255).required(),
+        password: Joi.string().min(6).max(1024).required(),
+    });
+
+    return schema.validate(data);
+}
+
 module.exports = {
-    registerValidation
+    registerValidation,
+    loginValidation
 }
