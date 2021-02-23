7501f78331bf85f54995a3e2e2f97f48675d0cde
debubz <bao.es.hoang2@gmail.com>
2021-02-23 16:03:10 +1100

    comleted content and starting assessments

--- learn/udem/sec3_basics/notes.md
+++ learn/udem/sec3_basics/notes.md
@@ -1,3 +1,47 @@
 # some generic notes going through the thing
 
 
+> lets also do components 
+
+## state manipulation
+
+- set state only mutate the specified prop unlike hooks
+- but you can have multiple hooks
+
+## stateful v stateless
+
+one manage state and other doesnt
+
+- good to split components so some stateful manage logic
+  - stateless manage jsx
+
+> split back to class?
+
+## passing method references between components
+
+> binding is weird
+
+- technically its similar to annonymous funcs
+  - apparently it is bad for performance if using annonymous functions
+
+## two way binding
+
+- this might be ok to try
+- helps better create creating stateless components
+
+> wait Im already doing this
+
+## aight some practice assignment
+
+- [] Create TWO new components: UserInput and UserOutput
+- [] UserInput should hold an input element, UserOutput two paragraphs
+- [] Output multiple UserOutput components in the App component (any paragraph texts of your choice)
+- [] Pass a username (of your choice) to UserOutput via props and display it there
+- [] Add state to the App component (=> the username) and pass the username to the UserOutput component
+- [] Add a method to manipulate the state (=> an event-handler method)
+- [] Pass the event-handler method reference to the UserInput component and bind it to the input-change event
+- [] Ensure that the new input entered by the user overwrites the old username passed to UserOutput
+- [] Add two-way-binding to your input (in UserInput) to also display the starting username
+- [] Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets
+
+
--- learn/udem/sec3_basics/something_old/src/App.js
+++ learn/udem/sec3_basics/something_old/src/App.js
@@ -1,18 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Person from "./person/Person";
 
 class App extends Component {
+  // all of this is only for components
+  // data
+  state = {
+    persons: [
+      { name: "max", age: 3 },
+      { name: "cheese", age: 10 },
+      { name: "sticks", age: 20 },
+    ],
+  };
+
+  mapPersons = () =>
+    this.state.persons.map((e, i) => (
+      <Person name={e.name} age={e.age} change={this.nameChange} key={i} />
+    ));
+
+  // clicky
+  click = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 3 },
+        { name: "cheese", age: 10 },
+      ],
+    });
+  };
+
+  nameChange = (e) => {
+    this.setState({
+      persons: [
+        { name: "max", age: 3 },
+        { name: e.target.value, age: 10 },
+        { name: "sticks", age: 20 },
+      ],
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button style={style} onClick={() => this.click("yuck")}>
+          click me
+        </button>
+        {this.mapPersons()}
+        <Person name="click" age="321" click={this.click.bind(this, "steve")} />
       </div>
     );
   }
