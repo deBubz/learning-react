422340fc3dad9c0fcbc13f03c917e0fef197680d
debubz <bao.es.hoang2@gmail.com>
2021-06-20 08:01:57 +1000

    removing react stuff

--- react/redux/redux-todoapp/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import visibilityFilter from './visibilityFilter'
-import todos from './todos'
-
-// main reducer i think
-
-export default combineReducers({ todos, visibilityFilter })
